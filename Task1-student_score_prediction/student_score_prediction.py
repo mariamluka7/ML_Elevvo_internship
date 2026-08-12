ecfa3545855a9f04ce197b58ba2e06ce301d8587
Mariam Gamal <mariamluka103@gmail.com>
2026-08-12 23:01:49 +0300

    uncommenting prints so the code is functioning

--- Task1-student_score_prediction/student_score_prediction.py
+++ Task1-student_score_prediction/student_score_prediction.py
@@ -8,19 +8,19 @@ from sklearn.metrics import mean_squared_error, r2_score
 
 data = pd.read_csv("Task1-student_score_prediction/StudentPerformanceFactors.csv")
 
-#print(data.head())
-#print(data.describe())
-#print(data.info())
-#print(data.isnull().sum())
-#print(data.duplicated().sum())
+print(data.head())
+print(data.describe())
+print(data.info())
+print(data.isnull().sum())
+print(data.duplicated().sum())
 
 data = data.dropna()
 
-# plt.scatter(data['Hours_Studied'], data['Exam_Score'])
-# plt.xlabel('Hours Studied')
-# plt.ylabel('Exam Score')
-# plt.title('Relationship between Hours Studied and Exam Score')
-# plt.show()
+plt.scatter(data['Hours_Studied'], data['Exam_Score'])
+plt.xlabel('Hours Studied')
+plt.ylabel('Exam Score')
+plt.title('Relationship between Hours Studied and Exam Score')
+plt.show()
 
 X = data[['Hours_Studied']]
 y = data['Exam_Score']
@@ -33,18 +33,18 @@ model.fit(X_train, y_train)
 predictions = model.predict(X_test)
 
 mse = mean_squared_error(y_test, predictions)
-#print(f"Mean Squared Error: {mse}") #--> 12.351733799445533
+print(f"Mean Squared Error: {mse}") #--> 12.351733799445533
 
 r2 = r2_score(y_test, predictions)
-#print(f"R-squared: {r2}") #--> 0.20513060832466712
+print(f"R-squared: {r2}") #--> 0.20513060832466712
 
 plt.scatter(y_test, predictions)
 plt.xlabel('Actual Scores')
 plt.ylabel('Predicted Scores')
 plt.title('Actual vs Predicted Scores')
-#plt.show()
+plt.show()
 
-# print(data["Hours_Studied"].corr(data["Exam_Score"])) #--> 0.44510414026511613
+print(f"Correlation between Hours Studied and Exam Score: {data['Hours_Studied'].corr(data['Exam_Score'])}") #--> 0.44510414026511613
 
-# print(model.coef_) #--> [0.28834271]
-# print(model.intercept_) #--> 61.48999442610166
+print(f"Model Coefficient: {model.coef_[0]}") #--> 0.28834271
+print(f"Model Intercept: {model.intercept_}") #--> 61.48999442610166
